fb25fc6692f651fcdadc4fe22a45513f4a034463
Vitaly Parkhamovich <melodeath44@mail.ru>
2023-08-12 22:27:18 +0300

    init: start task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function toReadable (number) {
-  
+    const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 1, 20];
+    const numStr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
+    'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen',
+     'eighteen','nineteen', 'twenty']
+
+  if(num[number]){
+    return numStr[number]
+
+
+  }
 }
