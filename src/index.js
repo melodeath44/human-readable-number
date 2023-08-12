d8487f13b8a90a9b4f5996bababb49f9fe38f345
Vitaly Parkhamovich <melodeath44@mail.ru>
2023-08-12 23:20:36 +0300

    feat: add new text js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-module.exports = function toReadable (number) {
-    const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 1, 20];
+module.exports = function toReadable(number) {
+    const num = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
     const numStr = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
     'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen',
      'eighteen','nineteen', 'twenty']
@@ -7,12 +7,10 @@ module.exports = function toReadable (number) {
     const numStr1 = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight',
        'nine']
 
-    if(num[number]){
-    const numOne = numStr[number]
-    return numOne;
-        }
-
-    else if (number > 20 && number <= 29) {
+    if(num[number]) {
+        const numOne = numStr[number];
+        return numOne;
+    } else if (number > 20 && number <= 29) {
         return `twenty ${numStr1[number % 10]}`;
-        }
+    }
 }
