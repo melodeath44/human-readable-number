ff7e8f0ad4efeae71604594b102b32f951429ff9
Vitaly Parkhamovich <melodeath44@mail.ru>
2023-08-13 12:13:20 +0300

    feat: add new text js

--- src/index.js
+++ src/index.js
@@ -46,30 +46,44 @@ module.exports = function toReadable(number) {
         const lastNum = number % 10;
         const str = numStr1[lastNum - 1];
         return `twenty ${str}`;
+    } else if (number === 30) {
+        return `thirty`;
     } else if (number >= 31 && number <= 39) {
         const lastNum = number % 10;
         const str = numStr1[lastNum - 1];
         return `thirty ${str}`;
+    } else if (number === 40) {
+        return `forty`;
     } else if (number >= 41 && number <= 49) {
         const lastNum = number % 10;
         const str = numStr1[lastNum - 1];
         return `forty ${str}`;
+    } else if (number === 50) {
+        return `fifty`;
     } else if (number >= 51 && number <= 59) {
         const lastNum = number % 10;
         const str = numStr1[lastNum - 1];
         return `fifty ${str}`;
+    } else if (number === 60) {
+        return `sixty`;
     } else if (number >= 61 && number <= 69) {
         const lastNum = number % 10;
         const str = numStr1[lastNum - 1];
         return `sixty ${str}`;
+    } else if (number === 70) {
+        return `seventy`;
     } else if (number >= 71 && number <= 79) {
         const lastNum = number % 10;
         const str = numStr1[lastNum - 1];
         return `seventy ${str}`;
+    } else if (number === 80) {
+        return `eighty`;
     } else if (number >= 81 && number <= 89) {
         const lastNum = number % 10;
         const str = numStr1[lastNum - 1];
         return `eighty ${str}`;
+    } else if (number === 90) {
+        return `ninety`;
     } else if (number >= 91 && number <= 99) {
         const lastNum = number % 10;
         const str = numStr1[lastNum - 1];
