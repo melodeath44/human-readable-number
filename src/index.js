881ed93d7da3221accad761278ecea8bcad7dfe3
Vitaly Parkhamovich <melodeath44@mail.ru>
2023-08-13 12:58:18 +0300

    feat: add new text js

--- src/index.js
+++ src/index.js
@@ -175,15 +175,14 @@ module.exports = function toReadable(number) {
         "one hundred"
     ];
 
-    const newNumSplit = number.split('');
-    const newNumShift = newNumSplit.shift();
-    const newNumJoin = newNumShift.shift('');
+    
 
     if (num[number]) {
         const numOne = numStr[number];
         return numOne;
     }
     else if(number >= 101 && number <=199){
-        return `one hundred ${numStr[newNumJoin]}`
+        const newNum = toString().number.substring(1);
+        return `one hundred ${numStr[newNum]}`
     }
 };
