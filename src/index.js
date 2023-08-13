7d948bd5431259c9372c5b4679313cc62444f0e7
Vitaly Parkhamovich <melodeath44@mail.ru>
2023-08-13 13:31:28 +0300

    feat: add new text js

--- src/index.js
+++ src/index.js
@@ -185,6 +185,6 @@ module.exports = function toReadable(number) {
         const newNumStr = number.toString();
         const newNumSlice = newNumStr.slice(1);
         const finishNum = +newNumSlice;
-        return `${numStr[101]} ${numStr[finishNum]}`;
+        return `${numStr[100]} ${numStr[finishNum]}`;
     }
 };
